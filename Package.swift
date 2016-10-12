c721217dd3a365b6b0bf90ae3c8175c1e222720f
Sam Coles <SamuelCodes@users.noreply.github.com>
2016-10-12 19:33:01 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "HDF5Kit",
     dependencies: [
-        .Package(url: "https://github.com/aleph7/CHDF5.git", majorVersion: 1)
+        .Package(url: "https://github.com/SamuelCodes/CHDF5.git", majorVersion: 1)
     ]
 )
